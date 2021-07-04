2dba936507cbb96d936355444b9764a05e97ddbb
SCP-EEE <scpeee@wail.ch>
2021-07-05 08:55:00 +0900

    list_up

--- src/main.c
+++ src/main.c
@@ -6,6 +6,9 @@
 #define one_code_size 200
 #define stack_size 10
 #define list_size 50
+#define val_size 30
+
+char nil[] = "nil";
 
 char *cdr(char *list)
 {
@@ -13,7 +16,9 @@ char *cdr(char *list)
   bool flag=0;    
   list = index(list, '(');
   while(list[0] != '\0'){
-    printf("%d %d %c\n", depth, flag, list[0]);
+    #ifndef DEBUG
+    printf("depth:%d flag:%d %c\n", depth, flag, list[0]);
+    #endif
     switch (list[0]){
     case '(':
       if(depth==1 && flag==1){ (list-1)[0]='('; return list=list-1; }
@@ -37,7 +42,9 @@ char *car(char *list)
   bool flag=0;    
   list = index(list, '(');
   while(list[i] != '\0'){
-    printf("%d %d %c\n", depth, flag, list[i]);
+    #ifndef DEBUG
+    printf("depth:%d flag:%d %c\n", depth, flag, list[i]);
+    #endif
     switch (list[i]){
     case '(':
       if(depth==1 && flag==1){ list[0] = ' '; /* list[i-1]=')'; */ list[i] = '\0'; return list;}
@@ -54,21 +61,40 @@ char *car(char *list)
   return list;
 }
 
+void list_up(char *check_list, char *list, char **list_val){
+  unsigned char depth=0;
+  unsigned int list_index=0, n=0, m=0, chklist_index=0;
+  bool flag=0;
+  list = index(list, '(');
+  while(check_list[chklist_index] != '\0'){
+    #ifndef DEBUG
+    printf("depth:%d flag:%d %c\n", depth, flag, check_list[chklist_index]);
+    #endif
+    switch (check_list[chklist_index]){
+    case '(':
+      if(depth==1 && flag==1){  }
+      list[list_index] = list[2+list_index];
+      list[1+list_index] = *nil;
+      depth+=1;
+      break;
+    case ')': depth-=1; break;
+    case ' ':
+      if(depth==1 && flag==1){  }
+      break;
+    default: if(check_list[chklist_index] != ' ' && check_list[chklist_index] != '('){ list_val[m==val_size ? n++ : n][m==val_size ? m=0 : m++] = check_list[chklist_index]; list[strcmp(list[list_index], "nil") ? 2+list_index : list_index] = list_val[m-1]; } if(depth>=1){ flag=1; } break;
+    }
+    list_index+=1;
+  }
+}
+
 int main(void)
 {
-  char list_val[list_size][30];
+  char list_val[list_size][val_size];
   char *list[list_size];
   int S[stack_size];
   char *E[list_size];
   char *C[one_code_size];
   char *D[list_size];
   char list_test[]="((a) (b (c)))";
-  strcpy(list_val[0], list_test);
-  car(list_val[0]);
-  list[0] = list_val[0];
-  strcpy(list_val[1], list_test);
-  cdr(list_val[1]);
-  list[1] = list_val[1];
-  printf("%s %s", list[0], list[1]);
   return 0;
 }
